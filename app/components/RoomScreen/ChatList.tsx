14469d76b23a5e40c7b340ac1424a73628cbca37
mdi80 <mdi80nz@gmail.com>
2023-09-22 13:55:56 +0330

    create date of day for chat list

--- app/components/RoomScreen/ChatList.tsx
+++ app/components/RoomScreen/ChatList.tsx
@@ -8,6 +8,9 @@ import { AppDispatch, RootState } from '../../store'
 import { ChatType } from '../../types'
 import useTheme from '../../theme'
 import ChatItem from './ChatListItem'
+import { getDateOfMessage, isDatesInSameDay } from './utils'
+import Text from '../Text'
+import typogrphy from '../../theme/font'
 
 
 const RoomChatList = ({ username }: { username: string }) => {
@@ -20,14 +23,49 @@ const RoomChatList = ({ username }: { username: string }) => {
 
     const saveMessage = (currentUsername === username)
 
-    const renderItem = ({ item }: { item: ChatType }) => {
+    const renderItem = ({ item, index }: { item: ChatType, index: number }) => {
+        //@ts-ignore
+        const per = chats[index + 1]
 
-        return <ChatItem
-            seen={item.seen}
-            sendToServer={item.saved}
-            time={item.date}
-            message={item.message}
-            self={(saveMessage || item.from_user !== username)} />
+        return (
+            <>
+                <ChatItem
+                    seen={item.seen}
+                    sendToServer={item.saved}
+                    time={item.date}
+                    message={item.message}
+                    self={(saveMessage || item.from_user !== username)} />
+                {per ?
+                    !isDatesInSameDay(per.date, item.date) &&
+                    <Text
+                        style={{
+                            alignSelf: 'center',
+                            fontSize: typogrphy.fontSize.sm,
+                            backgroundColor: '#bbbbbbaa',
+                            paddingVertical: 3,
+                            paddingHorizontal: 10,
+                            borderRadius: 20,
+                            color: 'white',
+                        }}
+                    >{getDateOfMessage(item.date)}</Text>
+                    :
+
+                    <Text
+                        style={{
+                            alignSelf: 'center',
+                            fontSize: typogrphy.fontSize.sm,
+                            backgroundColor: '#bbbbbbaa',
+                            paddingVertical: 3,
+                            paddingHorizontal: 10,
+                            borderRadius: 20,
+                            color: 'white',
+                        }}
+                    >{getDateOfMessage(item.date)}</Text>
+
+                }
+
+            </>
+        )
     }
 
     return (
--- app/components/RoomScreen/utils.tsx
+++ app/components/RoomScreen/utils.tsx
@@ -1,11 +1,44 @@
+import { ChatType } from "../../types"
+
+export function isDatesInSameDay(t1: number, t2: number) {
+
+    const date1 = new Date(t1)
+    const date2 = new Date(t2)
+
+    return date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear();
+
+}
+
+export function getDateOfMessage(t: number) {
+    const date = new Date(t)
+
+    const now = new Date()
+    const mid = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    const midLastWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7)
+    const midLastDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
+
+    if (mid.getTime() < date.getTime()) {
+        return "Today"
+    } else if (midLastDay.getTime() < date.getTime()) {
+        return "Yesterday"
+    } else if (midLastWeek.getTime() < date.getTime()) {
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return days[date.getDay()]
+    } else {
+    }
+
+}
+
 
 
 export function getDateFromTimeStampForLastSeen(t: number): string {
     const date = new Date(t * 1000)
     const now = new Date()
     const mid = new Date(now.getFullYear(), now.getMonth(), now.getDate())
-    const midLastWeek = new Date(now.getFullYear(), now.getMonth() - 7, now.getDate())
-    const midLastDay = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate())
+    const midLastWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7)
+    const midLastDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1)
 
     if (mid.getTime() < date.getTime()) {
 
@@ -30,4 +63,15 @@ function formatTime(date: Date) {
     const hours = String(date.getHours())
     const minutes = String(date.getMinutes()).padStart(2, '0');
     return `${hours}:${minutes}`;
+}
+
+function formatDate(date: Date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = String(date.getFullYear())
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+
+    return `${monthNames[date.getMonth()]} ${day},${year}`;
 }
